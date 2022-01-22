b48935450d07f7795988760922bbb53530728a5e
AmaniAbedAlRaheem <amani.abed24@gmail.com>
2022-01-22 17:26:52 +0200

    Js wait until Dom loaded , Initialized JS file with function names

---
+++ game.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+    document.getElementById("start").addEventListener("click",function(){
+        StartGame();
+
+    });
+
+
+    document.getElementById("end").addEventListener('onmouseover',function(){
+        EndGame();
+    });
+
+
+});
+    
+
+
+
+
