cb4cb12fd101f8689630e3a8bcb9f5e499e8aead
AmaniAbedAlRaheem <amani.abed24@gmail.com>
2022-01-23 20:28:45 +0200

    Beautified code

--- game.js
+++ game.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function (event)
 
     });
 
-    //onmouseover : not working , re-check
     document.getElementById("end").addEventListener("mouseover", function () {
         ReachedEnd();
 
