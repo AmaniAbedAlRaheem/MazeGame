84688285b0d08590386598f4f697fb7343778fff
AmaniAbedAlRaheem <amani.abed24@gmail.com>
2022-01-24 13:25:31 +0200

    Testing code update on github

--- game.js
+++ game.js
@@ -71,10 +71,7 @@ function ReachedEnd() {
 
 function Boundaries() {
 
-    if (score != 0){
-    score -= 10;
-    document.getElementsByClassName("example")[0].innerHTML = score;
-    }
+ 
 
     var boundaries = document.getElementsByClassName("boundary");
 
