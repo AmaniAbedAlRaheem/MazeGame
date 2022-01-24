7c862189fa57c3f25a829695de25f4907b14486e
AmaniAbedAlRaheem <amani.abed24@gmail.com>
2022-01-24 12:48:31 +0200

    Beautified File

--- game.js
+++ game.js
@@ -1,35 +1,22 @@
-document.addEventListener("DOMContentLoaded", function (event) 
-{   
-
-
-  
+document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("start").addEventListener("click", function () {
-   
-        StartGame();
-   
 
+        StartGame();
     });
 
     document.getElementById("end").addEventListener("mouseover", function () {
         ReachedEnd();
 
-     
-
-
     });
 
 
     var boundaries = document.querySelectorAll(".boundary");
-    for (var i=0 ; i <boundaries.length; i++){
-        
+    for (var i = 0; i < boundaries.length; i++) {
+
         boundaries[i].onmouseover = Boundaries;
-   
 
     }
-
-    
-    
 
 });
 
@@ -37,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function (event)
 function StartGame() {
 
 
-    document.getElementsByClassName("example")[0].innerHTML =0;
+    document.getElementsByClassName("example")[0].innerHTML = 0;
     //In order to loop on all the boundary divs
     var boundaries = document.getElementsByClassName("boundary");
 
@@ -47,72 +34,47 @@ function StartGame() {
     {
         boundaries[i].style.backgroundColor = "#eeeeee";
 
-
-      
         if (boundaries[i].style.backgroundColor != "red")
 
         {
-           
+
             var state = document.getElementById("status").innerHTML = 'Begin by moving your mouse over the "S".';
-
-
         }
 
 
     }
 
-
 }
-
-
-
 
 function ReachedEnd() {
 
     var score = 0;
- 
-    var boundaries = document.getElementsByClassName("boundary");
-  
 
+    var boundaries = document.getElementsByClassName("boundary");
 
     for (var i = 0; i < boundaries.length; i++) {
-
-
 
         if (boundaries[i].style.backgroundColor != "red") {
 
             var state = document.getElementById('status').innerHTML = " You Won ";
-            document.getElementsByClassName("example")[0].innerHTML =  score +5;
-            
-            
-        
+            document.getElementsByClassName("example")[0].innerHTML = score + 5;
+
         }
     }
 }
 
-
-
-
-function Boundaries(){
-    
+function Boundaries() {
 
     var boundaries = document.getElementsByClassName("boundary");
-  
-    for (var i=0; i<boundaries.length; i++){
+
+    for (var i = 0; i < boundaries.length; i++) {
 
         boundaries[i].style.backgroundColor = "red";
-        
-      
 
-        if(boundaries[i].style.backgroundColor =="red")
-        {
+        if (boundaries[i].style.backgroundColor == "red") {
 
             var state = document.getElementById('status').innerHTML = "You Lost";
-            
-            
-            
-            
+
         }
     }
 }
-
